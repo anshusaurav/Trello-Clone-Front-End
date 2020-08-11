cdfb393a92dd3eb4ffcd1f8326f555980a87174a
anshusaurav <anshu.saurav@gmail.com>
2020-08-11 15:20:53 +0530

    Login Page Update

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import { Button, Form, Grid, Divider, Message, Segment } from 'semantic-ui-react'
+import { Button, Form, Grid, Divider, Segment } from 'semantic-ui-react'
 import CustomFormHeader from './../components/common/CustomFormHeader'
+import leftBG from './../images/leftbg.svg'
+import rightBG from './../images/rightbg.svg'
 class SignInForm extends React.Component {
     render() {
         return (
             <div className='form-container'>
-                <Grid textAlign='center' style={{ height: '100vh' }
-                } verticalAlign='middle' segment>
-                    <Grid.Column style={{ maxWidth: 450 }}>
-                        <CustomFormHeader />
+                <div className='form-container-outer-div'>
+                    <CustomFormHeader />
+                    <div className='form-container-inner-div' >
+                        <h2>Log in to Trello</h2>
+
                         <Segment style={{ padding: '1rem' }}>
                             <Form size='large'>
 
@@ -26,16 +29,23 @@ class SignInForm extends React.Component {
                             </Form>
                             <Divider inverted />
                             <div className='form-extra-links'>
-                                <a href='#'>Can't log in?</a>
-                                <a href='#'>Sign up for an account</a>
+                                <a href='#' className='form-link'>Can't log in?</a>
+                                <a href='#' className='form-link'>Sign up for an account</a>
                             </div>
                         </Segment>
-                    </Grid.Column>
-                </Grid>
-                <div className="background">
-
+                    </div>
                 </div>
-            </div>
+                <div className="background">
+                    <div className="background-inner">
+                        <div className='leftLarge'>
+                            <img alt="TrelloBG" src="https://cdn4.buysellads.net/uu/1/41334/1550855391-cc_dark.png"></img>
+                        </div>
+                        <div className='rightLarge'>
+                            <img alt="TrelloBG" src={rightBG}></img>
+                        </div>
+                    </div>
+                </div>
+            </div >
         )
     }
 }
--- src/components/common/CustomFormHeader.js
+++ src/components/common/CustomFormHeader.js
@@ -3,7 +3,7 @@ import logo from './../../trello-logo-blue.svg'
 class CustomFormHeader extends React.Component {
     render() {
         return (
-            <img alt="Trello" class="trello-main-logo" src={logo}></img>
+            <img alt="Trello" className="trello-main-logo" src={logo}></img>
         )
     }
 }
