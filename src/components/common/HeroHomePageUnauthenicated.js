43bf74ee12366ebf31f869f0a727fcda928c0d0b
anshusaurav <anshu.saurav@gmail.com>
2020-08-13 16:36:17 +0530

    Improvisation in Home and Login SignUp page

--- src/components/common/HeroHomePageUnauthenicated.js
+++ src/components/common/HeroHomePageUnauthenicated.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { Input, Button } from 'semantic-ui-react'
+// import { Link } from 'react-router-dom'
+import { Input } from 'semantic-ui-react'
 class HeroHomePageUnauthenicated extends React.Component {
     render() {
         return (<section className="hero-home-page">
             <div className="hero-home-page-container">
                 <div className="hero-home-inner-div">
-                    <div class="hero-home-inner-text">
+                    <div className="hero-home-inner-text">
                         <h1>Trello lets you work more collaboratively and get more done.</h1>
-                        <p class="lead">Trello’s boards, lists, and cards enable you to organize and prioritize your projects in a fun, flexible, and rewarding way.</p>
+                        <p className="lead">Trello’s boards, lists, and cards enable you to organize and prioritize your projects in a fun, flexible, and rewarding way.</p>
                     </div>
                     <div className="hero-home-inner-img">
                         <img src="https://d2k1ftgv7pobq7.cloudfront.net/meta/p/res/images/308998dcb3ed5ab3d01217a4d24ffa03/hero-a.svg" width="582" class="img-fluid" alt="" />
