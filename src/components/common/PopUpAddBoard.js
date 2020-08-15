87abe4d8a444752ad7c77eddf6f23e487d4761f0
anshusaurav <anshu.saurav@gmail.com>
2020-08-15 13:17:31 +0530

    SCSS files reorganized

---
+++ src/components/common/PopUpAddBoard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Card } from 'semantic-ui-react'
+import imgSrc from './../../images/add-team-one.svg'
+class PopUpAddTeam extends React.Component {
+
+    render() {
+
+        return (
+            <Card className="popup-add-board-form-card">
+
+
+
+            </Card>
+        )
+    }
+}
+export default PopUpAddTeam
