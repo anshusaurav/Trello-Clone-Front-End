79c5bb2a74843d6745722058bdaa976942e67270
anshusaurav <anshu.saurav@gmail.com>
2020-08-15 16:22:43 +0530

    Add Board Pop Up

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import './../src/scss/index.scss'
 import 'semantic-ui-css/semantic.min.css'
 import Main from './../src/components/Main'
-import PopUpAddTeam from './components/common/PopUpAddTeam';
+import PopUpAddTeam from './components/common/PopUpAddTeam'
+import PopUpAddBoard from './components/common/PopUpAddBoard'
 
 function App() {
   return (
     // <Main />
-    <PopUpAddTeam />
+    <PopUpAddBoard />
   );
 }
 
--- src/components/common/PopUpAddBoard.js
+++ src/components/common/PopUpAddBoard.js
@@ -1,17 +1,110 @@
 import React from 'react'
-import { Card } from 'semantic-ui-react'
-import imgSrc from './../../images/add-team-one.svg'
-class PopUpAddTeam extends React.Component {
+import { Dropdown, Icon } from 'semantic-ui-react'
+class PopUpAddBoard extends React.Component {
 
     render() {
+        const teams = [
+            {
+                key: 'No Team',
+                text: 'No Team',
+                value: 'No Team',
+            },
+            {
+                key: 'Jenny Hess',
+                text: 'Jenny Hess',
+                value: 'Jenny Hess',
+            },
+            {
+                key: 'Elliot Fu',
+                text: 'Elliot Fu',
+                value: 'Elliot Fu',
+            },
+            {
+                key: 'Stevie Feliciano',
+                text: 'Stevie Feliciano',
+                value: 'Stevie Feliciano',
 
+            },
+            {
+                key: 'Elliot Fu',
+                text: 'Elliot Fu',
+                value: 'Elliot Fu',
+            },
+            {
+                key: 'Stevie Feliciano',
+                text: 'Stevie Feliciano',
+                value: 'Stevie Feliciano',
+
+            },
+
+        ]
+        const arrBG = ['https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+            'https://images.pexels.com/photos/870711/pexels-photo-870711.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+            'https://images.pexels.com/photos/196667/pexels-photo-196667.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+            'https://images.pexels.com/photos/186980/pexels-photo-186980.jpeg?cs=srgb&dl=pexels-tahir-shaw-186980.jpg&fm=jpg',
+            'https://images.pexels.com/photos/753626/pexels-photo-753626.jpeg?cs=srgb&dl=pexels-julius-silver-753626.jpg&fm=jpg',
+            'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+            'https://images.pexels.com/photos/870711/pexels-photo-870711.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+            'https://images.pexels.com/photos/196667/pexels-photo-196667.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+            'https://images.pexels.com/photos/186980/pexels-photo-186980.jpeg?cs=srgb&dl=pexels-tahir-shaw-186980.jpg&fm=jpg',]
         return (
-            <Card className="popup-add-board-form-card">
+            <div className="popup-add-board-form-card">
+                <div className="popup-add-board-outer">
+                    <div className="popup-add-board-inner">
+                        <form className="create-board-form">
+                            <div className="create-board-form-container">
+                                <div className="create-board-tile">
+                                    <button className="create-board-popup-close-btn">
+                                        X
+                                    </button>
+                                    <div>
+                                        <input placeholder="Add board title" className="board-pop-up-input"></input>
+                                    </div>
+                                    <div>
+                                        <Dropdown
+                                            inline
+                                            options={teams}
+                                            defaultValue={teams[0].value}
+
+                                        />
+                                    </div>
+                                    <div>
+                                        <button className='add-board-private-btn'>
+                                            <Icon name="lock" className="add-board-i-btn"></Icon>
+                                            Private
+
+                                        </button>
+                                        <button className='add-board-private-btn'>
+                                            <Icon name="globe" className="add-board-i-btn"></Icon>
+                                            Public
+
+                                        </button>
+                                    </div>
+                                </div>
+                                <ul className="create-board-background">
+                                    {
+                                        arrBG.map(elem => {
+                                            return (<li className="create-board-background-item">
+                                                <button className='create-board-bg-item-btn' style={{
+                                                    backgroundImage: `url(${elem})`
+                                                }}>
+
+                                                </button>
+                                            </li>)
+                                        })
+                                    }
+                                </ul>
+                            </div>
+                            <div className="action-items">
+                                <button className='add-board-submit-btn'>Create Board</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
 
 
-
-            </Card>
+            </div>
         )
     }
 }
-export default PopUpAddTeam
+export default PopUpAddBoard
