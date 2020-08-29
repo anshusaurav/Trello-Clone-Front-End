f43657570ffe021db37ab7f60c262ef57573cbf6
anshusaurav <anshu.saurav@gmail.com>
2020-08-29 22:41:19 +0530

    Minor fixes

--- src/redux/actionTypes.js
+++
@@ -1,4 +0,0 @@
-
-export const FETCH_USER = "FETCH_USER";
-export const UPDATE_USER = "UPDATE_USER";
-export const REMOVE_USER = "REMOVE_USER";
--- src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from "redux";
-import rootReducer from "./../redux/reducers";
-
-export default createStore(
-  rootReducer /* preloadedState, */,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
